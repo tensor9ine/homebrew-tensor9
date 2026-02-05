2c3311222aee03891c43b408bfcb117edb5dc65e
mtenpow <mtp@tensor9.com>
2026-02-05 05:10:03 -0800

    Update to bundled JRE distribution (2026_02_05_13_04_8c63b2cc)
    - Switched from GraalVM native binary to bundled Temurin JRE
    - Bundle includes: JRE + t9min jar + launcher script
    - Size: ~364MB (includes JRE)
    - Mac ARM64 SHA256: 8c63b2cc3ab053a1f7661d000cbb2337905c3760b8ea9ba1e286e7d5be249740

--- Formula/tensor9.rb
+++ Formula/tensor9.rb
@@ -1,64 +1,63 @@
 # Tensor9 CLI - Deploy anywhere with Tensor9
 # https://tensor9.com
 #
-# Install: brew install tensor9ine/homebrew/tensor9
+# Install: brew tap tensor9ine/tensor9 && brew install tensor9
 # Update:  brew upgrade tensor9
 
 class Tensor9 < Formula
   desc "Deploy anywhere with Tensor9 - multi-cloud deployment platform"
   homepage "https://tensor9.com"
-  version "2026.02.04-9ab28221"
+  version "2026_02_05_13_04_8c63b2cc"
   license :cannot_represent
 
-  # Multi-architecture support
+  # Multi-architecture support - bundled JRE tar.gz archives
   on_macos do
     on_arm do
-      url "https://t9-artifacts-prod-1.s3.us-west-2.amazonaws.com/tensor9_mac_arm64-2026_02_04_23_14_9ab28221"
-      sha256 "9ab2822156b4946a20455f61dc7dd1ab55298c2d0149498fe66878b2ee4cfe8f"
+      url "https://t9-artifacts-prod-1.s3.us-west-2.amazonaws.com/tensor9_mac_arm64-2026_02_05_13_04_8c63b2cc.tar.gz"
+      sha256 "8c63b2cc3ab053a1f7661d000cbb2337905c3760b8ea9ba1e286e7d5be249740"
     end
     on_intel do
-      url "https://t9-artifacts-prod-1.s3.us-west-2.amazonaws.com/tensor9_mac_amd64-latest"
-      sha256 "PLACEHOLDER_SHA256_AMD64"
+      url "https://t9-artifacts-prod-1.s3.us-west-2.amazonaws.com/tensor9_mac_amd64-latest.tar.gz"
+      sha256 "PENDING_BUILD"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://t9-artifacts-prod-1.s3.us-west-2.amazonaws.com/tensor9_linux_arm64-latest"
-      sha256 "PLACEHOLDER_SHA256_LINUX_ARM64"
+      url "https://t9-artifacts-prod-1.s3.us-west-2.amazonaws.com/tensor9_linux_arm64-latest.tar.gz"
+      sha256 "PENDING_BUILD"
     end
     on_intel do
-      url "https://t9-artifacts-prod-1.s3.us-west-2.amazonaws.com/tensor9_linux_amd64-latest"
-      sha256 "PLACEHOLDER_SHA256_LINUX_AMD64"
+      url "https://t9-artifacts-prod-1.s3.us-west-2.amazonaws.com/tensor9_linux_amd64-latest.tar.gz"
+      sha256 "PENDING_BUILD"
     end
   end
 
   def install
-    # The downloaded file is a raw binary, rename to tensor9
-    binary_name = if Hardware::CPU.arm? && OS.mac?
-      "tensor9_mac_arm64-2026_02_04_23_14_9ab28221"
-    elsif Hardware::CPU.intel? && OS.mac?
-      "tensor9_mac_amd64-latest"
-    elsif Hardware::CPU.arm? && OS.linux?
-      "tensor9_linux_arm64-latest"
-    elsif Hardware::CPU.intel? && OS.linux?
-      "tensor9_linux_amd64-latest"
-    end
+    # The tarball extracts to tensor9-{platform}_{arch}/
+    # containing: bin/tensor9, lib/t9min-*.jar, jre/
 
-    bin.install binary_name => "tensor9"
+    # Install entire bundle to libexec (Homebrew convention for private deps)
+    libexec.install Dir["*"]
+
+    # Create wrapper script that calls the bundled launcher
+    bin.write_exec_script libexec/"bin/tensor9"
   end
 
   def caveats
     <<~EOS
-      To get started, run:
-        tensor9 setup
+      tensor9 has been installed with a bundled Java runtime.
+      No separate Java installation is required.
 
-      For help:
-        tensor9 help
+      Get started:
+        tensor9 --help
+
+      Configuration is stored in:
+        ~/.tensor9/
     EOS
   end
 
   test do
-    system "#{bin}/tensor9", "env"
+    assert_match "tensor9", shell_output("#{bin}/tensor9 --help")
   end
 end
