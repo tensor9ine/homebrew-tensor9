292bc558337076731ab699a7e62448a43b76cc1e
mtenpow <mtp@tensor9.com>
2026-04-06 03:43:20 -0700

    bump CLI to 2026_04_06_10_18_ff16cb6e

--- Formula/tensor9.rb
+++ Formula/tensor9.rb
@@ -7,29 +7,29 @@
 class Tensor9 < Formula
   desc "Deploy anywhere with Tensor9 - multi-cloud deployment platform"
   homepage "https://tensor9.com"
-  version "2026_04_06_02_44_14d58bca"
+  version "2026_04_06_10_18_ff16cb6e"
   license :cannot_represent
 
   # Multi-architecture support - bundled JRE tar.gz archives
   on_macos do
     on_arm do
-      url "https://t9-artifacts-prod-1.s3.us-west-2.amazonaws.com/tensor9_mac_arm64-2026_04_06_02_44_14d58bca.tar.gz"
-      sha256 "14d58bca859f5d895097a647a841ebcae2fa5625893a4677208a7c468e2d3d8c"
+      url "https://t9-artifacts-prod-1.s3.us-west-2.amazonaws.com/tensor9_mac_arm64-2026_04_06_10_18_ff16cb6e.tar.gz"
+      sha256 "ff16cb6ec2d621e67594004bcdb14b9e0eb69ac339127f5ff12c6e741e9127fb"
     end
     on_intel do
-      url "https://t9-artifacts-prod-1.s3.us-west-2.amazonaws.com/tensor9_mac_amd64-2026_04_06_02_45_f72583e4.tar.gz"
-      sha256 "f72583e4edc16b917ad1cbfdf1186d19785535319fe8c62847640bcb889e8b36"
+      url "https://t9-artifacts-prod-1.s3.us-west-2.amazonaws.com/tensor9_mac_amd64-2026_04_06_10_19_9e4c741d.tar.gz"
+      sha256 "9e4c741d353c3fc5aecaee8c3f2f10e9099e99eb68e1b1066bfea8fc064333c9"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://t9-artifacts-prod-1.s3.us-west-2.amazonaws.com/tensor9_linux_arm64-2026_04_06_02_45_59b44722.tar.gz"
-      sha256 "59b4472220a1d7d9d8f68c4a9d26cf3f61c44b37d5a787b655757b2a048bd26e"
+      url "https://t9-artifacts-prod-1.s3.us-west-2.amazonaws.com/tensor9_linux_arm64-2026_04_06_10_19_4daf5da0.tar.gz"
+      sha256 "4daf5da0272033916c69af73386c32eaba4f0afb07ee93983d9d7f93f0c614ae"
     end
     on_intel do
-      url "https://t9-artifacts-prod-1.s3.us-west-2.amazonaws.com/tensor9_linux_amd64-2026_04_06_02_46_a55e2ad3.tar.gz"
-      sha256 "a55e2ad33d9bcfedbf99a15c1f8e6fc2e662f152cd0f8e9a6c372dfc86271a8f"
+      url "https://t9-artifacts-prod-1.s3.us-west-2.amazonaws.com/tensor9_linux_amd64-2026_04_06_10_20_a53afc85.tar.gz"
+      sha256 "a53afc85bced5ef791a6cbb63a756aa838c5e1b3d00c20db5c13a92974ef425e"
     end
   end
 
